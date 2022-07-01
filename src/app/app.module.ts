f5caa3d28a19cccbe535e8c83506ac8033893665
Collin Bartley <collinbartley1@gmail.com>
2022-07-01 12:24:49 -0400

    Creating some components, and setting up the basic page layout.
    Bio component, Entry component, page layout stuff, and some styling stuff too.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BioComponent } from './components/bio/bio.component';
+import { EntryComponent } from './components/entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
