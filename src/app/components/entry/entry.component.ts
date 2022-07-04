25cb253a74a59fb88e4dc1b5f827b997f95c3281
Collin Bartley <collinbartley1@gmail.com>
2022-07-04 17:37:25 -0400

    Finished setting up the components and added some content
    Layout is still responsive and looks great. There are a few things that break at certain resolutions, so that's my next step.

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { formatDate } from "@angular/common";
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.scss']
 })
-export class EntryComponent implements OnInit {
+export class EntryComponent {
+
+  @Input() entryType: 'work' | 'school' = 'work';
+  @Input() title: string = '';
+  @Input() set startDate(val: string) {
+    if(val) {
+      this.start = formatDate(val, this.format, 'en-US');
+    }
+  };
+  @Input() set endDate(val: string) {
+    if(val) {
+      this.end = formatDate(val, this.format, 'en-US');
+    }
+  };
+  @Input() iconPath: string = '';
+
+  public start: string = '';
+  public end: string = 'Present';
+
+  public format: string = 'MMMM yyyy';
 
   constructor() { }
 
-  ngOnInit(): void {
+  amountOfTime(start: string, end: string){
+    end == 'Present' ? end = new Date().toString() : end = end;
+    let startingDate = new Date(start);
+    let endingDate = new Date(end);
+
+    return ((Date.UTC(endingDate.getFullYear(), endingDate.getMonth(), endingDate.getDate()) - Date.UTC(startingDate.getFullYear(), startingDate.getMonth(), startingDate.getDate()) ) /(1000 * 60 * 60 * 24) / 365).toFixed(1);
   }
 
 }
